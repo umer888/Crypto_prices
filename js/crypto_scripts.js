9f1ec4946fd35836b6dd31b4b5d5d8a20c27f2a9
umer hayat <33321888+umer888@users.noreply.github.com>
2019-07-27 16:55:16 +0500

    The assets of the plugin

---
+++ js/crypto_scripts.js
@@ -0,0 +1,10 @@
+if(!window.jQuery)
+{
+   var script = document.createElement('script');
+   script.type = "text/javascript";
+   script.src = "https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js";
+   document.getElementsByTagName('head')[0].appendChild(script);
+}
+
+
+
